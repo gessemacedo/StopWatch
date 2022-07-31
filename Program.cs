6a6588986d857c773d1479516f5fe9cd0cccca8c
gesse.macedo-sc <gesse.soares.macedo@gmail.com>
2022-07-31 00:04:04 -0300

    finalizado projeto

--- Program.cs
+++ Program.cs
@@ -19,11 +19,32 @@ namespace MyApp
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            Console.WriteLine(data);
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+                multiplier = 60;
 
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
         }
 
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
         static void Start(int time)
         {
             int currentTime = 0;
@@ -38,6 +59,7 @@ namespace MyApp
             Console.Clear();
             Console.WriteLine("StopWatch Finalizado.");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
